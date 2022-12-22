d6f87f64af5a7742226de899d098f2008fcbdc79
vuxuanthanh1203 <vuxuanthanh1203@gmail.com>
2022-12-22 16:57:40 +0700

    export list product to csv

--- Server/app.js
+++ Server/app.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const express = require('express')
+const { ApolloServer } = require('apollo-server-express')
 
-const app = express();
+const app = express()
 
 // Load Schemas & resolves
-const typeDefs = require('./schemas/schema');
-const resolvers = require('./resolvers/resolver');
+const typeDefs = require('./schemas/schema')
+const resolvers = require('./resolvers/resolver')
 
 // Load query method
-const queryData = require('./data/queryData');
+const queryData = require('./data/queryData')
+
+// export data
+const exportData = require('./export/exportData')
+app.use('./public', express.static(__dirname + './public'))
 
 const startApolloServer = async () => {
-    const server = new ApolloServer({ typeDefs, resolvers, context: () => ({ queryData }) });
-    await server.start();
+  const server = new ApolloServer({ typeDefs, resolvers, context: () => ({ queryData }) })
+  await server.start()
 
-    server.applyMiddleware({ app });
+  server.applyMiddleware({ app })
 }
 
-startApolloServer();
-
-
+startApolloServer()
+exportData()
 app.listen(4000, () => {
-    console.log("Server running on port 4000");
-});
+  console.log('Server running on port 4000')
+})
