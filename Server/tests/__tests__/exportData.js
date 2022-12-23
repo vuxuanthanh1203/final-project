e564dfa18c8977df2f025bd9fb1683c429885f2c
vuxuanthanh1203 <vuxuanthanh1203@gmail.com>
2022-12-23 14:26:25 +0700

    add text export file

--- Server/tests/__tests__/exportData.js
+++
@@ -1,9 +0,0 @@
-const exportData = require('../../export/exportData')
-
-describe('testExportData', () => {
-  test('test export prduct data', async () => {
-    const result = await exportData()
-
-    expect(result).toMatch(/.csv/)
-  })
-})
